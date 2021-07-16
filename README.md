c17b4883e535b7dd173590c8b66c1fe2e9db8760
praveenpunati <praveen.punati@gmail.com>
2021-07-16 05:01:17 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,32 +1,14 @@
-# StackOver flow data results
-Lets Review what we see in stack over flow data trends from last couple years
+# Disaster Response Pipeline Project
 
-we will use data available from 2011 to 2020 survey results from across the globe and address business problem solutions
+### Instructions:
+1. Run the following commands in the project's root directory to set up your database and model.
 
+    - To run ETL pipeline that cleans data and stores in database
+        `python data/process_data.py data/disaster_messages.csv data/disaster_categories.csv data/DisasterResponse.db`
+    - To run ML pipeline that trains classifier and saves
+        `python models/train_classifier.py data/DisasterResponse.db models/classifier.pkl`
 
+2. Run the following command in the app's directory to run your web app.
+    `python run.py`
 
-Lets provide some insights from the data available by using Exploratory data analysis using python and jupyter note book.
-
-You can find complete python code from my github url 
-
-Lets Review 3 key elements with in the data
-
-1 ) Survey questions 
-
-Over the period starting from 2011 to 2020 format of response collection is different hence analyzing this type of data will be difficult as there are very few common question across the years .
-
-Also see that from latest years trend is leaning towards more work life balance and social media related question we need to wait for couple years to better understand these .
-
-Survey questions needs to be streamlined for questions that are being replaced keep them and collect survey results as zero response
-
-2) Respondents  regions or countries 
-
-from the data we see top 4 contributors are from USA , India, UnitedKingdom an canda however recent trend suggests that lot of users are coming from Germany as well along with India reaching to second spot
-
-3) Responses
-
-when it comes to responses see USA and India all the way into top spots however for year 6 UK came to top2 
-
-
-
-Also we can perform deep analytics into this data by profession , age ,income and i will encourage you to start with my note book which can be found in github link 
+3. Go to http://0.0.0.0:3001/
---
+++ train_classifier.py
@@ -0,0 +1,123 @@
+# import libraries
+import sys
+import pandas as pd
+import numpy as np
+import nltk
+import pickle
+
+from sqlalchemy import create_engine
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+
+from sklearn.metrics import confusion_matrix, classification_report
+# from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.pipeline import Pipeline, FeatureUnion
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.ensemble import AdaBoostClassifier
+
+
+nltk.download(['punkt', 'wordnet'])
+
+def load_data(database_filepath):
+    '''
+    Loads Data
+    '''
+    # load data from database
+    engine = create_engine(f'sqlite:///{database_filepath}')
+    df = pd.read_sql('SELECT * FROM processed_df', engine)
+    X = df['message']
+    y = df.iloc[:, 4:]
+    category_names = list(df.columns[4:])
+    return X, y, category_names
+
+
+def tokenize(text):
+    '''
+    Tokenizes the text data provided
+    '''
+    tokens = word_tokenize(text)
+    lemmatizer = WordNetLemmatizer()
+
+    clean_tokens = []
+    for tok in tokens:
+        clean_tok = lemmatizer.lemmatize(tok).lower().strip()
+        clean_tokens.append(clean_tok)
+
+    return clean_tokens
+
+
+def build_model():
+    '''
+    Builds the model
+    '''
+    pipeline_ada = Pipeline([
+        ('features', FeatureUnion([
+
+            ('text_pipeline', Pipeline([
+                ('vect', CountVectorizer(tokenizer=tokenize)),
+                ('tfidf', TfidfTransformer())
+            ]))
+        ])),
+
+        ('clf', MultiOutputClassifier(AdaBoostClassifier()))
+    ])
+
+    parameters = {
+                  "clf__estimator__n_estimators": [50],
+                  "clf__estimator__learning_rate": [0.1, 1]
+                 }
+    cv_ada = GridSearchCV(pipeline_ada, param_grid=parameters)
+    return cv_ada
+
+
+def evaluate_model(model, X_test, Y_test, category_names):
+    '''
+    Evaluates the model
+    '''
+    y_pred = model.predict(X_test)
+    cols = Y_test.columns
+    for i in range(y_pred.shape[-1]):
+        print(f"\n\nOutput Category: {cols[i]}")
+        print(classification_report(np.array(Y_test[cols[i]]), y_pred[:, i]))
+
+
+def save_model(model, model_filepath):
+    '''
+    Saves the trained model as pickle file
+    '''
+    pickle.dump(model, open(model_filepath, 'wb'))
+
+
+def main():
+    if len(sys.argv) == 3:
+        database_filepath, model_filepath = sys.argv[1:]
+        print('Loading data...\n    DATABASE: {}'.format(database_filepath))
+        X, Y, category_names = load_data(database_filepath)
+        X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
+        
+        print('Building model...')
+        model = build_model()
+        
+        print('Training model...')
+        model.fit(X_train, Y_train)
+        
+        print('Evaluating model...')
+        evaluate_model(model, X_test, Y_test, category_names)
+
+        print('Saving model...\n    MODEL: {}'.format(model_filepath))
+        save_model(model, model_filepath)
+
+        print('Trained model saved!')
+
+    else:
+        print('Please provide the filepath of the disaster messages database '\
+              'as the first argument and the filepath of the pickle file to '\
+              'save the model to as the second argument. \n\nExample: python '\
+              'train_classifier.py ../data/DisasterResponse.db classifier.pkl')
+
+
+if __name__ == '__main__':
+    main()
